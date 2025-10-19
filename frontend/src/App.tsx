7636c921724e8eedb1ee2bb4cb4f8e018c92a247
Esteves, Kalei (Student) <ke131@students.waikato.ac.nz>
2025-10-19 17:48:23 +1300

    Added memoji mp4, and some placeholder text.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,7 +4,23 @@ function App() {
 
   return (
     <>
-        <h1>Hello, World</h1>
+        <h1>Hello,</h1>
+        <p>It's LeiLei here ;)</p>
+        <video
+            autoPlay
+            loop
+            muted
+            playsInline
+            style={{
+                width: '400px',
+                imageRendering: 'auto',
+                transform: 'translateZ(0)',
+            }}
+        >
+            <source src="/memoji.mp4" type="video/mp4" />
+        </video>
+        <h3>This is a placeholder portrait!</h3>
+        <p>Disclaimer: I haven't graduated yet, but let a girl dream.</p>
     </>
   )
 }
